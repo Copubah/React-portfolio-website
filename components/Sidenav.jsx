dfd9f71d7f848c54d592ce77248633ae89021cd5
Devopuba <charlesopuba3@gmail.com>
2023-04-09 14:38:04 +0300

    changes

--- components/Sidenav.jsx
+++ components/Sidenav.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import{AiOutlineMenu} from 'react-icons/ai'
+import React,{useState} from 'react'
+import{AiOutlineHome, AiOutlineMenu} from 'react-icons/ai'
 
-function Sidenav() {
+const Sidenav =() => {
+
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav);
+    };
   return (
     <div>
-        <AiOutlineMenu/>
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div className='fixed w-full h-sceen bg-white/90 flex flex-col justify-center items-center z-20'>
+                    <a href="#main">
+                    <AiOutlineHome size={20}/>
+                    <span className='pl-4'>Home</span>
+
+                    </a>
+                </div>    
+            )
+        
+        }
+
     </div>
-  )
-}
+  );
+};
 
 export default Sidenav
